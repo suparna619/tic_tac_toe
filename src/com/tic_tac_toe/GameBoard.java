ae7d7148265202b4296ed3faf95be459f784a762
Suparna <suparnad@thoughtworks.com>
2015-03-26 15:37:51 +0530

    library modified

--- src/com/tic_tac_toe/GameBoard.java
+++ src/com/tic_tac_toe/GameBoard.java
@@ -1,66 +1,61 @@
 package com.tic_tac_toe;
 
-/**
- * Created by suparnad on 3/26/2015.
- */
 public class GameBoard {
     private int numOfRow = 3;
     private int numOfColumn = 3;
     private String[][] board = new String[numOfColumn][numOfRow];
-    private int row = 0;
-    private int column = 0;
 
 
-    public void printBoard(int x, int y, String playerSymbol, String playerName){
-        if (x>numOfRow-1 || y>numOfColumn-1) {
-            System.out.println("Wrong position");
-            System.exit(1);
-        }
+    public void playTicTacToe(int x, int y, String playerSymbol, String playerName){
+        ifWrongPosition(x, y);
 
         board[x][y] = playerSymbol;
-        if(checkConditionToWin(playerSymbol)){
-            System.out.println("***Congratulation "+playerName+" wines***");
-            System.exit(1);
-        }
+        winMessage(playerSymbol, playerName);
         for (int i = 0; i < numOfColumn; i++){
             for (int j = 0; j < numOfRow; j++){
                 if(board[i][j] != null)
                     System.out.print("  "+board[i][j]+"  ");
                 else System.out.print("  -  ");
             }
-            System.out.println("\n");
+            System.out.println(System.lineSeparator());
         }
 
     }
 
+    private void winMessage(String playerSymbol, String playerName) {
+        if(checkConditionToWin(playerSymbol)){
+            System.out.println("***Congratulation "+playerName+" wines***");
+            System.exit(1);
+        }
+    }
+
+    private void ifWrongPosition(int x, int y) {
+        if (x>numOfRow-1 || y>numOfColumn-1) {
+            System.out.println("Wrong position");
+            System.exit(1);
+        }
+    }
+
     private boolean checkConditionToWin(String playerSymbol) {
-        if(checkForRow(playerSymbol)
-                || checkForColumn(playerSymbol) || checkForDiagonal(playerSymbol))
-            return true;
-        else return false;
+        return checkForRow(playerSymbol)
+                || checkForColumn(playerSymbol) || checkForDiagonal(playerSymbol);
     }
 
     private boolean checkForDiagonal(String playerSymbol) {
-        if ((board[0][0]==playerSymbol && board[1][1]==playerSymbol && board[2][2]==playerSymbol) ||
-                (board[0][2]==playerSymbol && board[1][1]==playerSymbol && board[2][0]==playerSymbol))
-            return true;
-        else return false;
+        return (board[0][0].equals(playerSymbol) && board[1][1].equals(playerSymbol) && board[2][2].equals(playerSymbol)) ||
+                (board[0][2].equals(playerSymbol) && board[1][1].equals(playerSymbol) && board[2][0].equals(playerSymbol));
     }
 
     private boolean checkForColumn(String playerSymbol) {
-        if ((board[0][0]==playerSymbol && board[1][0]==playerSymbol && board[2][0]==playerSymbol) ||
-                (board[0][1]==playerSymbol && board[1][1]==playerSymbol && board[2][1]==playerSymbol) ||
-                (board[0][2]==playerSymbol && board[1][2]==playerSymbol && board[2][2]==playerSymbol))
-            return true;
-        else return false;
+        return (board[0][0].equals(playerSymbol) && board[1][0].equals(playerSymbol) && board[2][0].equals(playerSymbol)) ||
+                (board[0][1].equals(playerSymbol) && board[1][1].equals(playerSymbol) && board[2][1].equals(playerSymbol)) ||
+                (board[0][2].equals(playerSymbol) && board[1][2].equals(playerSymbol) && board[2][2].equals(playerSymbol));
     }
 
     private boolean checkForRow(String playerSymbol) {
-        if ((board[0][0]==playerSymbol && board[0][1]==playerSymbol && board[0][2]==playerSymbol) ||
-                (board[1][0]==playerSymbol && board[1][1]==playerSymbol && board[1][2]==playerSymbol) ||
-                (board[2][0]==playerSymbol && board[2][1]==playerSymbol && board[2][2]==playerSymbol))
-            return true;
-        else return false;
+        return (board[0][0].equals(playerSymbol) && board[0][1].equals(playerSymbol) && board[0][2].equals(playerSymbol)) ||
+                (board[1][0].equals(playerSymbol) && board[1][1].equals(playerSymbol) && board[1][2].equals(playerSymbol)) ||
+                (board[2][0].equals(playerSymbol) && board[2][1].equals(playerSymbol) && board[2][2].equals(playerSymbol));
     }
 
 }
